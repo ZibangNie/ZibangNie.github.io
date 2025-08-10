255199b59bb88f969784a29c2e6416a6d1379e06
Zibang Nie <2542881@dundee.ac.uk>
2025-08-10 15:03:49 +0800

    Add basic home page

---
+++ HomePage/home.js
@@ -0,0 +1,5 @@
+// JavaScript for smooth animation and content display
+window.onload = function() {
+    const homePage = document.querySelector('.home-container');
+    homePage.classList.add('fadeIn');
+};
